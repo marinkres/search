8464bcef7fd61456734a6c175c17e053a48231f3
marinkres <marin.kresic1@gmail.com>
2023-05-29 20:56:39 +0200

    e

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -6,7 +6,6 @@ export const GET = async (request) => {
         await connectToDB()
 
         const prompts = await Prompt.find({}).populate('creator')
-        console.log('PAZNJA')
         return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
         return new Response("Failed to fetch all prompt", { status: 500 })
