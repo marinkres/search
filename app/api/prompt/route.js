1166eee46589a5356991dcc91fdcb82f50167e9b
marinkres <marin.kresic1@gmail.com>
2023-05-30 15:42:28 +0200

    populate test

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,20 +4,22 @@ import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
 export const GET = async (request) => {
-    console.log("BLABLABLA")
     try {
         await connectToDB()
 
-        const prompts = await Prompt.find({}).populate('creator')
+        const prompts = await Prompt.find({}).populate({
+            path: "creator",
+            model: User,
+          });
         return new Response(JSON.stringify(prompts), { status: 200,
             headers: {
                 "Cache-Control": "s-maxage=0, stale-while-revalidate"
-            }, })
+            }, });
         
     } catch (error) {
         return new Response("Failed to fetch all prompt", { status: 500,
             headers: {
                 "Cache-Control": "s-maxage=0, stale-while-revalidate"
-            },  })
+            },  });
     }
-} 
+}; 
