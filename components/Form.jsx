0e6c544e756ac4acc7dbe5528e7b6eb19d875de8
marinkres <marin.kresic1@gmail.com>
2023-06-03 09:58:36 +0200

    rating design

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,85 +1,132 @@
 import Link from "next/link";
 import CloseIcon from '@mui/icons-material/Close';
 import Rating from '@mui/material/Rating';
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied';
+import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAltOutlined';
+import SentimentVerySatisfiedIcon from '@mui/icons-material/SentimentVerySatisfied';
+import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
+
+const StyledRating = styled(Rating)(({ theme }) => ({
+  '& .MuiRating-iconEmpty .MuiSvgIcon-root': {
+    color: theme.palette.action.disabled,
+  },
+}));
+
+const customIcons = {
+  1: {
+    icon: <SentimentVeryDissatisfiedIcon color="error" />,
+    label: 'Very Dissatisfied',
+  },
+  2: {
+    icon: <SentimentDissatisfiedIcon color="error" />,
+    label: 'Dissatisfied',
+  },
+  3: {
+    icon: <SentimentSatisfiedIcon color="warning" />,
+    label: 'Neutral',
+  },
+  4: {
+    icon: <SentimentSatisfiedAltIcon color="success" />,
+    label: 'Satisfied',
+  },
+  5: {
+    icon: <SentimentVerySatisfiedIcon color="success" />,
+    label: 'Very Satisfied',
+  },
+};
+
+function IconContainer(props) {
+  const { value, ...other } = props;
+  return <span {...other}>{customIcons[value].icon}</span>;
+}
+
+IconContainer.propTypes = {
+  value: PropTypes.number.isRequired,
+};
 
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
-  return (
-    <section className='w-full max-w-full flex-start flex-col'>
-      <h1 className='head_text text-left'>
-        <span className='bluemoje'>{type} Sranje</span>
-      </h1>
-      <p className='desc text-left max-w-md'>
-        {type} i podijeli sa drugima svoje muke i kritike.
-      </p>
+ return (
+ <section className='w-full max-w-full flex-start flex-col'>
+ <h1 className='head_text text-left'>
+ <span className='bluemoje'>{type} Sranje</span>
+ </h1>
+ <p className='desc text-left max-w-md'>
+ {type} i podijeli sa drugima svoje muke i kritike.
+ </p>
 
-      <form
-        onSubmit={handleSubmit}
-        className='mt-10 w-full max-w-2xl flex flex-col gap-7 '
-      >
-        <label>
-          <span className='font-satoshi font-semibold text-base text-gray-300'>
-            Tvoja objava:
-          </span>
+ <form
+ onSubmit={handleSubmit}
+ className='mt-10 w-full max-w-2xl flex flex-col gap-7 '
+ >
+ <label>
+ <span className='font-satoshi font-semibold text-base text-gray-300'>
+ Tvoja objava:
+ </span>
 
-          <textarea
-            value={post.prompt}
-            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
-            placeholder='Poseri se tu...'
-            required
-            className='form_textarea '
-            style={{ backgroundColor: '#202225', borderColor: "#23272a", color: 'white'}}
-          />
-        </label>
+ <textarea
+ value={post.prompt}
+ onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+ placeholder='Poseri se tu...'
+ required
+ className='form_textarea '
+ style={{ backgroundColor: '#202225', borderColor: "#23272a", color: 'white'}}
+ />
+ </label>
 
-        <label>
-          <span className='font-satoshi font-semibold text-base text-gray-300'>
-            Tagovi{" "}
-            <span className='font-normal'>
-              (#Sto god)
-            </span>
-          </span>
-          <input
-            value={post.tag}
-            onChange={(e) => setPost({ ...post, tag: e.target.value })}
-            type='text'
-            placeholder='#Tag'
-            required
-            className='form_input'
-            style={{ backgroundColor: '#202225', borderColor: "#23272a", color: 'white'}}
-          />
-        </label>
-        <label>
-            <span className='font-satoshi font-semibold text-base text-gray-300'>
-              Rating:
-            </span>
-            <div>
-              <Rating
-                name="rating"
-                value={post.rating}
-                onChange={(event, newValue) => {
-                  setPost({ ...post, rating: newValue });
-                }}
-              />
-            </div>
-          </label>
+ <label>
+ <span className='font-satoshi font-semibold text-base text-gray-300'>
+ Tagovi{" "}
+ <span className='font-normal'>
+ (#Sto god)
+ </span>
+ </span>
+ <input
+ value={post.tag}
+ onChange={(e) => setPost({ ...post, tag: e.target.value })}
+ type='text'
+ placeholder='#Tag'
+ required
+ className='form_input'
+ style={{ backgroundColor: '#202225', borderColor: "#23272a", color: 'white'}}
+ />
+ </label>
+ <label>
+ <span className='font-satoshi font-semibold text-base text-gray-300'>
+ Rating:
+ </span>
+ <div className="mt-2 "> 
+   <StyledRating
+     name="rating"
+     value={post.rating}
+     onChange={(event, newValue) => {
+       setPost({ ...post, rating: newValue });
+     }}
+     IconContainerComponent={IconContainer}
+     getLabelText={(value) => customIcons[value].label}
+   />
+ </div>
+ </label>
 
-        <div className='flex-end mx-3 mb-5 gap-4'>
-          <Link href='/' className='crveno text-sm'>
-          <CloseIcon />
-          </Link>
+ <div className='flex-end mx-3 mb-5 gap-4'>
+ <Link href='/' className='crveno text-sm'>
+ <CloseIcon />
+ </Link>
 
-          <button
-            type='submit'
-            disabled={submitting}
-            className='px-5 py-1.5 text-sm rounded-full text-white'
-            style={{ backgroundColor: '#5865f2'}}
-          >
-            {submitting ? `${type}ing...` : type}
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+ <button
+ type='submit'
+ disabled={submitting}
+ className='px-5 py-1.5 text-sm rounded-full text-white'
+ style={{ backgroundColor: '#5865f2'}}
+ >
+ {submitting ? `${type}ing...` : type}
+ </button>
+ </div>
+ </form>
+ </section>
+ );
 };
 
 export default Form;
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,109 +5,160 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 import { Person } from "@mui/icons-material";
+import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
 import Rating from '@mui/material/Rating';
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied';
+import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAltOutlined';
+import SentimentVerySatisfiedIcon from '@mui/icons-material/SentimentVerySatisfied';
+
+const StyledRating = styled(Rating)(({ theme }) => ({
+  '& .MuiRating-iconEmpty .MuiSvgIcon-root': {
+    color: theme.palette.action.disabled,
+  },
+}));
+
+const customIcons = {
+  1: {
+    icon: <SentimentVeryDissatisfiedIcon color="error" />,
+    label: 'Very Dissatisfied',
+  },
+  2: {
+    icon: <SentimentDissatisfiedIcon color="error" />,
+    label: 'Dissatisfied',
+  },
+  3: {
+    icon: <SentimentSatisfiedIcon color="warning" />,
+    label: 'Neutral',
+  },
+  4: {
+    icon: <SentimentSatisfiedAltIcon color="success" />,
+    label: 'Satisfied',
+  },
+  5: {
+    icon: <SentimentVerySatisfiedIcon color="success" />,
+    label: 'Very Satisfied',
+  },
+};
+
+function IconContainer(props) {
+  const { value, ...other } = props;
+  return <span {...other}>{customIcons[value].icon}</span>;
+}
+
+IconContainer.propTypes = {
+  value: PropTypes.number.isRequired,
+};
 
 const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
-  const { data: session } = useSession();
-  const pathName = usePathname();
-  const router = useRouter();
+ const { data: session } = useSession();
+ const pathName = usePathname();
+ const router = useRouter();
 
-  const [copied, setCopied] = useState("");
+ const [copied, setCopied] = useState("");
 
-  const handleProfileClick = () => {
-    console.log(post);
+ const handleProfileClick = () => {
+ console.log(post);
 
-    if (post.creator._id === session?.user.id) return router.push("/profile");
+ if (post.creator._id === session?.user.id) return router.push("/profile");
 
-    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
-  };
+ router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+ };
 
-  const handleCopy = () => {
-    setCopied(post.prompt);
-    navigator.clipboard.writeText(post.prompt);
-    setTimeout(() => setCopied(false), 3000);
-  };
-  const randomColor = () => {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
-  };
-  return (
-    <div className='prompt_card'>
-      <div className='flex justify-between items-start gap-5'>
-        <div
-          className='flex-1 flex justify-start items-center gap-3 cursor-pointer'
-          onClick={handleProfileClick}
-        >
-          <div
-            style={{
-              backgroundColor: randomColor(),
-              width: 40,
-              height: 40,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            className='rounded-full'
-          >
-            <Person style={{ color: "white" }} />
-          </div>
+ const handleCopy = () => {
+ setCopied(post.prompt);
+ navigator.clipboard.writeText(post.prompt);
+ setTimeout(() => setCopied(false), 3000);
+ };
+ const randomColor = () => {
+ const r = Math.floor(Math.random() * 256);
+ const g = Math.floor(Math.random() * 256);
+ const b = Math.floor(Math.random() * 256);
+ return `rgb(${r}, ${g}, ${b})`;
+ };
+ return (
+ <div className='prompt_card'>
+ <div className='flex justify-between items-start gap-5'>
+ <div
+ className='flex-1 flex justify-start items-center gap-3 cursor-pointer'
+ onClick={handleProfileClick}
+ >
+ <div
+ style={{
+ backgroundColor: randomColor(),
+ width: 40,
+ height: 40,
+ display: "flex",
+ justifyContent: "center",
+ alignItems: "center",
+ }}
+ className='rounded-full'
+ >
+ <Person style={{ color: "white" }} />
+ </div>
 
-          <div className='flex flex-col'>
-            <h3 className='font-satoshi font-semibold ' style={{color:'#FFF8F0'}}>
-              {post.creator.username}
-            </h3>
-            {/*
-            <p className='font-inter text-sm text-gray-500'>
-              {post.creator.email}
-            </p>
-            */}
-          </div>
-        </div>
+ <div className='flex flex-col'>
+ <h3 className='font-satoshi font-semibold ' style={{color:'#FFF8F0'}}>
+ {post.creator.username}
+ </h3>
+ {/*
+ <p className='font-inter text-sm text-gray-500'>
+ {post.creator.email}
+ </p>
+ */}
+ </div>
+ </div>
 
-        <div className='copy_btn' onClick={handleCopy}>
-          <Image
-            src={
-              copied === post.prompt
-                ? "/assets/icons/tick.svg"
-                : "/assets/icons/copy.svg"
-            }
-            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
-            width={12}
-            height={12}
-          />
-        </div>
-      </div>
+ <div className='copy_btn' onClick={handleCopy}>
+ <Image
+ src={
+ copied === post.prompt
+ ? "/assets/icons/tick.svg"
+ : "/assets/icons/copy.svg"
+ }
+ alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
+ width={12}
+ height={12}
+ />
+ </div>
+ </div>
 
-      <p className='my-4 font-satoshi text-sm text-gray-300'>{post.prompt}</p>
-      {/* Display the rating data using the Material-UI Rating component */}
-      <Rating name="read-only" value={post.rating} readOnly />
-      <p
-        className='font-inter text-sm bluemoje cursor-pointer'
-        onClick={() => handleTagClick && handleTagClick(post.tag)}
-      >
-        #{post.tag}
-      </p>
+ <p className='my-4 font-satoshi text-sm text-gray-300'>{post.prompt}</p>
+ {/* Display the rating data using the Material-UI Rating component */}
+   <StyledRating
+     name="read-only"
+     value={post.rating}
+     readOnly
+     IconContainerComponent={IconContainer}
+     getLabelText={(value) => customIcons[value]?.label ?? ''}
+   />
+ <p
+ className='font-inter text-sm bluemoje cursor-pointer'
+ onClick={() => handleTagClick && handleTagClick(post.tag)}
+ >
+ #{post.tag}
+ </p>
 
-      {session?.user.id === post.creator._id && pathName === "/profile" && (
-        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
-          <p
-            className='font-inter text-sm green_gradient cursor-pointer'
-            onClick={handleEdit}
-          >
-            Uredi
-          </p>
-          <p
-            className='font-inter text-sm crveno cursor-pointer'
-            onClick={handleDelete}
-          >
-            Obriši
-          </p>
-        </div>
-      )}
-    </div>
-  );
+ {session?.user.id === post.creator._id && pathName === "/profile" && (
+ <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+ <p
+ className='font-inter text-sm green_gradient cursor-pointer'
+ onClick={handleEdit}
+ >
+ Uredi
+ </p>
+ <p
+ className='font-inter text-sm crveno cursor-pointer'
+ onClick={handleDelete}
+ >
+ Obriši
+ </p>
+ </div>
+ )}
+ </div>
+ );
 };
 
 export default PromptCard;
