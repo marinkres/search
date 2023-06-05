93a955fdca13f4ec18868c5c1c7425993319066e
marinkres <marin.kresic1@gmail.com>
2023-06-05 13:50:39 +0200

    login termsofservice

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -40,83 +40,95 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
- {session?.user ? (
- <div className='flex gap-3 md:gap-5'>
- <Link href='/create-prompt' className='black_btn' 
-  style={{ backgroundColor: '#5865f2', borderColor: 'white' }}>
- Objavi
- </Link>
+    {session?.user ? (
+    <div className='flex gap-3 md:gap-5'>
+    <Link href='/create-prompt' className='black_btn' 
+      style={{ backgroundColor: '#5865f2', borderColor: 'white' }}>
+    Objavi
+    </Link>
 
- <button type='button' onClick={signOut} className='outline_btn'>
- Odjava
- </button>
+    <button type='button' onClick={signOut} className='outline_btn'>
+    Odjava
+    </button>
 
- <Link href='/profile'>
- <Image
- src={session?.user.image}
- width={37}
- height={37}
- className='rounded-full'
- alt='profile'
- />
- </Link>
- </div>
- ) : (
- <>
- <button
- type='button'
- onClick={() => setShowModal(true)}
- className='black_btn'
- style={{ backgroundColor: '#5865f2', borderColor: 'transparent' }}
- >
- Prijava
- </button>
+    <Link href='/profile'>
+    <Image
+    src={session?.user.image}
+    width={37}
+    height={37}
+    className='rounded-full'
+    alt='profile'
+    />
+    </Link>
+    </div>
+    ) : (
+    <>
+    <button
+    type='button'
+    onClick={() => setShowModal(true)}
+    className='black_btn'
+    style={{ backgroundColor: '#5865f2', borderColor: 'transparent' }}
+    >
+    Prijava
+    </button>
 
- {showModal && (
- <>
- <div
- className='fixed inset-0 z-50 bg-black bg-opacity-50'
- style={{ backdropFilter: 'blur(5px)' }}
- onClick={() => setShowModal(false)}
- ></div>
- <div className='fixed inset-0 z-50 flex items-center justify-center'>
- <div
- className='p-8 rounded-lg shadow-lg'
- style={{ backgroundColor: '#2c2f33' }}
- onClick={(e) => e.stopPropagation()}
- >
- {/* Modal content */}
- <div className='flex flex-col gap-4'>
- {providers &&
- Object.values(providers).map((provider) => (
- <button
- key={provider.name}
- onClick={() => {
- signIn(provider.id);
- }}
- style={{
- backgroundColor: 
- provider.name === 'Google' ? '' : '#5865f2',
- color: 'white',
- }}
- className='flex items-center gap-2 p-2 rounded-md  hover:bg-opacity-90'
- >
- {provider.name === 'Google' && <GoogleIcon />}
- {provider.name === 'Discord' && <DiscordIcon />}
- Prijava sa {provider.name}
- </button>
- ))}
- <button
- onClick={() => setShowModal(false)}
- className='p-2 rounded-md '
- style={{  color: '#99aab5' }}
- >
-  <CloseIcon />
-  Esc
- </button>
- </div>
- </div>
- </div>
+    {showModal && (
+    <>
+    <div
+    className='fixed inset-0 z-50 bg-black bg-opacity-50'
+    style={{ backdropFilter: 'blur(5px)' }}
+    onClick={() => setShowModal(false)}
+    ></div>
+    <div className='fixed inset-0 z-50 flex items-center justify-center'>
+    <div
+    className='p-8 rounded-lg shadow-lg'
+    style={{ backgroundColor: '#2c2f33' }}
+    onClick={(e) => e.stopPropagation()}
+    >
+    {/* Modal content */}
+      <div className='flex flex-col gap-4'>
+      {providers &&
+      Object.values(providers).map((provider) => (
+      <button
+      key={provider.name}
+      onClick={() => {
+      signIn(provider.id);
+      }}
+      style={{
+      backgroundColor: 
+      provider.name === 'Google' ? '' : '#5865f2',
+      color: 'white',
+      }}
+      className='flex items-center gap-2 p-2 rounded-md  hover:bg-opacity-90'
+      >
+      {provider.name === 'Google' && <GoogleIcon />}
+      {provider.name === 'Discord' && <DiscordIcon />}
+      Prijava sa {provider.name}
+      <style jsx>{`
+        button:hover {
+          background-color: grey;
+        }
+      `}</style>
+      </button>
+      ))}
+        <p className="text-center" style={{ color: '#99aab5' }}>
+          Prijavom na PoseriSe pristajete na naše{' '}
+          <a href="/assets/documents/terms_of_use_rejtajhr.pdf" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'underline' }}>
+            uvjete pružanja usluge
+          </a>
+          .
+        </p>
+      <button
+        onClick={() => setShowModal(false)}
+        className='p-2 rounded-md '
+        style={{  color: '#99aab5' }}
+        >
+          <CloseIcon />
+          Esc
+      </button>
+      </div>
+    </div>
+    </div>
  </>
  )}
  </>
@@ -182,6 +194,7 @@ const Nav = () => {
  >
  Odjava
  </button>
+ 
  <button
  onClick={() => setShowModal(false)}
  className='w-full '
@@ -220,27 +233,35 @@ const Nav = () => {
  style={{ backgroundColor: '#2c2f33' }}
  onClick={(e) => e.stopPropagation()}
  >
- {/* Modal content */}
- <div className='flex flex-col gap-4'>
- {providers &&
- Object.values(providers).map((provider) => (
- <button
- key={provider.name}
- onClick={() => {
- signIn(provider.id);
- }}
- style={{
- backgroundColor:
- provider.name === 'Google' ? '' : '#5865f2',
- color: 'white',
- }}
- className='flex items-center gap-2 p-2 rounded-md  hover:bg-opacity-90'
- >
- {provider.name === 'Google' && <GoogleIcon />}
- {provider.name === 'Discord' && <DiscordIcon />}
- Prijava sa {provider.name}
- </button>
+  {/* Modal content */}
+  <div className='flex flex-col gap-4'>
+    {providers &&
+    Object.values(providers).map((provider) => (
+    <button
+      key={provider.name}
+      onClick={() => {
+        signIn(provider.id);
+      }}
+      style={{
+        backgroundColor:
+        provider.name === 'Google' ? '' : '#5865f2',
+        color: 'white',
+      }}
+        className='flex items-center gap-2 p-2 rounded-md  hover:bg-opacity-90'
+      >
+        {provider.name === 'Google' && <GoogleIcon />}
+        {provider.name === 'Discord' && <DiscordIcon />}
+        Prijava sa {provider.name}
+        
+    </button>
  ))}
+ <p className="text-center" style={{ color: '#99aab5' }}>
+          Prijavom na PoseriSe pristajete na naše{' '}
+          <a href="/assets/documents/terms_of_use_rejtajhr.pdf" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'underline' }}>
+            uvjete pružanja usluge
+          </a>
+          .
+        </p>
  <button
  onClick={() => setShowModal(false)}
  className='p-2 rounded-md '
