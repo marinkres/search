a8e59b8da1e6e41d18729d4c762f6af3c138f741
marinkres <marin.kresic1@gmail.com>
2023-06-07 10:14:39 +0200

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <p style={{color: '#99aab5'}}>Copyright © 2023 Poseri se</p>
+      <p style={{color: '#99aab5', marginBottom: '10px'}}>Copyright © 2023 PoseriSe</p>
     </footer>
   );
 };
